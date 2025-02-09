4331c95865a875ce01a77e610b6cb2a42b81ab91
Thomas Maniero <thomas@maniero.info>
2025-02-10 00:04:28 +0100

    added phyBody type to body

--- plugins/spine/dist/SpineWebGLPluginDebug.js
+++ plugins/spine/dist/SpineWebGLPluginDebug.js
@@ -20941,7 +20941,7 @@ var GameObject = new Class({
          * If this Game Object is enabled for Arcade or Matter Physics then this property will contain a reference to a Physics Body.
          *
          * @name Phaser.GameObjects.GameObject#body
-         * @type {?(Phaser.Physics.Arcade.Body|Phaser.Physics.Arcade.StaticBody|MatterJS.BodyType)}
+         * @type {?(Phaser.Physics.Arcade.Body|Phaser.Physics.Arcade.StaticBody|MatterJS.BodyType|phyBody)}
          * @default null
          * @since 3.0.0
          */
